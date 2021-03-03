0d568c7cad107a3120821ee485d9dcc9dadad613
pradnya-wisdom <70446051+pradnya-wisdom@users.noreply.github.com>
2021-03-03 13:33:04 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,67 @@
+var starImg, fairyImg, bgImg;
+var fairy , fairyVoice;
+var star, starBody;
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	starImg = loadImage("images/star.png");
+	fairyImg = loadAnimation("images/fairyImage1.png","images/fairyImage2.png");
+	bgImg = loadImage("images/starNight.png");
+	fairyVoice = loadSound("sound/JoyMusic.mp3");
+
+}
+
+function setup() {
+	createCanvas(800, 750);
+
+	// fairyVoice.play();
+
+	fairy = createSprite(130, 520);
+	fairy.addAnimation("fairyflying",fairyImg);  
+	fairy.scale =0.25;
+
+	star = createSprite(650,30);
+	star.addImage(starImg);
+	star.scale = 0.2;
+
+	engine = Engine.create();
+	world = engine.world;
+
+	starBody = Bodies.circle(650 , 30 , 5 , {restitution:0.5, isStatic:true});
+	World.add(world, starBody);
+	
+	Engine.run(engine);
+
+}
+
+
+function draw() {
+  background(bgImg);
+Engine.update(engine)
+   if (keyDown("Left"))
+   {
+	   fairy.x=fairy.x-5
+
+   }
+   if (keyDown("Right"))
+   {
+	   fairy.x=fairy.x+5
+
+   }
+   if (keyDown("Space"))
+   {
+	starBody = Bodies.circle( {restitution:0.9, isStatic:false});
+
+   }
+  drawSprites();
+
+}
+
+function keyPressed(Left) {
+	fairy.x=fairy.x-5
+}
